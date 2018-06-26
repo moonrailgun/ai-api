00887dd716da26cccbd4c390ef221d9402e2a392
moonrailgun <moonrailgun@gmail.com>
2018-06-26 16:42:20 +0800

    增加高精度版本ocr

--- routes/ocr.py
+++ routes/ocr.py
@@ -114,3 +114,73 @@ def general_image():
         'result': True,
         'data': res,
     })
+
+@ocr.route('/accurate/base64', methods=['post'])
+def accurate_base64():
+    data = request.get_json()
+    base64str = data.get('base64')
+    detect_direction = data.get('detect_direction', False)
+    probability = data.get('probability', False)
+
+    if not base64str:
+        return jsonify({
+            'result': False,
+            'msg': u'缺少base64',
+        })
+
+    if ',' in base64str:
+        base64str = base64str.rsplit(',', 1)[1]
+
+    image = base64.b64decode(base64str) # 将base64解码
+
+    res = ocr_client.basicAccurate(image, {
+        'detect_direction': detect_direction,
+        'probability': probability,
+    })
+
+    if res.get('error_code'):
+        return jsonify({
+            'result': False,
+            'msg': res.get('error_msg', ''),
+        })
+
+    return jsonify({
+        'result': True,
+        'data': res,
+    })
+
+@ocr.route('/accurate/image', methods=['post'])
+def accurate_image():
+    image = request.files.get('image')
+    detect_direction = request.form.get('detect_direction', False)
+    probability = request.form.get('probability', False)
+
+    if not image:
+        return jsonify({
+            'result': False,
+            'msg': u'缺少image',
+        })
+
+    if not(image and allowed_file(image.filename)):
+        return jsonify({
+            'result': False,
+            'msg': u'文件上传失败,只允许jpg/png/bmp',
+        })
+
+    # 不存储
+    data = image.read()
+    res = ocr_client.basicAccurate(data, {
+        'detect_direction': detect_direction,
+        'probability': probability,
+    })
+
+    if res.get('error_code'):
+        return jsonify({
+            'result': False,
+            'msg': res.get('error_msg', ''),
+        })
+
+    return jsonify({
+        'result': True,
+        'data': res,
+    })
